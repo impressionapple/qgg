258f77fce534f187e984bf8bed1bd3f2831777e0
lwq <liwenquan@liwenquandeimac.local>
2017-03-29 21:07:16 +0800

    初始化项目

---
+++ qgg/qgg/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  qgg
+//
+//  Created by 李文全 on 17/3/29.
+//  Copyright © 2017年 李文全. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ qgg/qgg/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  qgg
+//
+//  Created by 李文全 on 17/3/29.
+//  Copyright © 2017年 李文全. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
